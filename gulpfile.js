6ba109128e7a47f8d45bb7707cb90e7bab3f8330
pip_lineupbuilder <howley.phil@gmail.com>
2016-11-17 16:30:31 -0800

    add gulpfile with sass, images, browser-sync info

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function(){
+  // **/*.scss - matches any file ending with .scss in the root folder and any child directories
+  return gulp.src('scss/**/*.scss')
+    // Converts Sass to CSS with gulp-sass using gulp-sass
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    // server: {
+    //   baseDir: '/'
+    // },
+    proxy: "localhost/ws-dom-final",
+    notify: false
+  });
+});
+
+gulp.task('images', function(){
+  return gulp.src('images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'images', 'fonts'],
+    callback
+  )
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+});
