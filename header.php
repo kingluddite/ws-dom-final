70a1112bf41781a63a1345cb80c656ba33fae116
pip_lineupbuilder <howley.phil@gmail.com>
2016-11-20 14:33:01 -0800

    add favicon

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
+  <link rel="icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
   <!-- Place favicon.ico in the root directory -->
 
   <?php wp_head(); ?>
