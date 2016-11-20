4458b744a17449047b3e86b0b707c0c160aa58e8
pip_lineupbuilder <howley.phil@gmail.com>
2016-11-20 14:26:19 -0800

    add basic styles

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-  <div id="navigation">
+  <nav>
     <ul>
       <li><a href="index.html">Home</a></li>
       <li><a href="about.html">About</a></li>
@@ -7,9 +7,9 @@
       <li><a href="live.html">Live</a></li>
       <li><a href="contact.html">Contact</a></li>
     </ul>
-  </div>
+  </nav>
   <div id="content">
     <h1>Welcome</h1>
-    <p id="intro">Welcome to the official website of Jay Skript and the Domsters. Here, you can <a href="about.html">learn more about the band</a>, view <a href="photos.html">photos of the band</a>, find out about <a href="live.html">tour dates</a> and <a href="contact.html">get in touch with the band</a>.</p>
+    <p id="intro" class="paragraph">Welcome to the official website of Jay Skript and the Domsters. Here, you can <a href="about.html">learn more about the band</a>, view <a href="photos.html">photos of the band</a>, find out about <a href="live.html">tour dates</a> and <a href="contact.html">get in touch with the band</a>.</p>
   </div>
 <?php get_footer(); ?>
