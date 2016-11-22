739cc4ab391fcf87c5ed1c770a362c00647daac2
pip_lineupbuilder <howley.phil@gmail.com>
2016-11-22 01:02:46 -0800

    update static nav

--- front-page.php
+++ front-page.php
@@ -1,18 +1,21 @@
 <?php get_header(); ?>
-  <nav>
-    <ul class="main-nav">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="photos.html">Photos</a></li>
-      <li><a href="live.html">Live</a></li>
-      <li><a href="contact.html">Contact</a></li>
-    </ul>
-  </nav>
+<nav>
+  <ul class="main-nav">
+    <li><a href="http://localhost:3000/ws-dom-final/">Home</a></li>
+    <li><a href="http://localhost:3000/ws-dom-final/about">About</a></li>
+    <li><a href="http://localhost:3000/ws-dom-final/photos">Photos</a></li>
+    <li><a href="http://localhost:3000/ws-dom-final/live">Live</a></li>
+    <li><a href="http://localhost:3000/ws-dom-final/contact">Contact</a></li>
+  </ul>
+</nav>
   <div class="content-container">
     <main class="main">
       <?php if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
       <?php endwhile; endif; ?>
+      <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
+      <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
+      <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
       <div class="gallery">
         <img src="http://placekitten.com/g/200/300" alt=""/>
         <img src="http://placekitten.com/g/200/300" alt=""/>
