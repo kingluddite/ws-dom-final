fc371485ea9bb8a27734231d85850f28ae7403d1
pip_lineupbuilder <howley.phil@gmail.com>
2016-11-23 12:25:24 -0800

    add images for photos and remove navbars from other pages

--- front-page.php
+++ front-page.php
@@ -1,13 +1,4 @@
 <?php get_header(); ?>
-<nav>
-  <ul class="main-nav">
-    <li><a href="http://localhost:3000/ws-dom-final/">Home</a></li>
-    <li><a href="http://localhost:3000/ws-dom-final/about">About</a></li>
-    <li><a href="http://localhost:3000/ws-dom-final/photos">Photos</a></li>
-    <li><a href="http://localhost:3000/ws-dom-final/live">Live</a></li>
-    <li><a href="http://localhost:3000/ws-dom-final/contact">Contact</a></li>
-  </ul>
-</nav>
   <div class="content-container">
     <main class="main">
       <?php if (have_posts() ) : while ( have_posts() ) : the_post(); ?>
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php get_header(); ?>
-  <nav>
-    <ul class="main-nav">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="photos.html">Photos</a></li>
-      <li><a href="live.html">Live</a></li>
-      <li><a href="contact.html">Contact</a></li>
-    </ul>
-  </nav>
   <div id="content">
     <h1>Welcome</h1>
     <p id="intro" class="paragraph">Welcome to the official website of Jay Skript and the Domsters. Here, you can <a href="about.html">learn more about the band</a>, view <a href="photos.html">photos of the band</a>, find out about <a href="live.html">tour dates</a> and <a href="contact.html">get in touch with the band</a>.</p>
